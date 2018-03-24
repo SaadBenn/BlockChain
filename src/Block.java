b93c0e7e6dff10c50a657688f09474da98ccad96
Saad <saadbennmushtaq@gmail.com>
2018-03-24 18:16:56 -0500

    Follow single line comment convention.

--- src/Block.java
+++ src/Block.java
@@ -4,8 +4,12 @@ public class Block {
 
     public String hash;
     public String prevHash;
-    private String data; // our data will be a simple message.
-    private long timeStamp; // as number of milliseconds since 1/1/1970.
+
+    // our data will be a simple message.
+    private String data;
+
+    // as number of milliseconds since 1/1/1970.
+    private long timeStamp;
     private int nonce;
 
     //Block Constructor.
@@ -14,7 +18,8 @@ public class Block {
         this.prevHash = prevHash;
         this.timeStamp = new Date().getTime();
 
-        this.hash = calculateHash(); // Making sure we do this after we set the other values.
+        // Making sure we do this after we set the other values.
+        this.hash = calculateHash();
     }
 
     public String calculateHash() {
@@ -23,7 +28,9 @@ public class Block {
     }
 
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
+
+        // Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
 
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
