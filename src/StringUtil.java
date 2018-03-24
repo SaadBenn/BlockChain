6dec6540340fb30338dbe9c35615944181b6ef58
Saad <saadbennmushtaq@gmail.com>
2018-03-24 18:18:48 -0500

    Apply the ECDSA to the message and add functionality for verifying the transaction.

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -1,17 +1,23 @@
-import java.security.MessageDigest;
+import java.security.*;
+import java.util.Base64;
+
 public class StringUtil {
 
-    //Applies Sha256 to a string and returns the result.
+    // Applies Sha256 to a string and returns the result.
     public static String applySha256(String data){
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-            //Applies sha256 to our data,
-            byte[] hash = digest.digest(data.getBytes("UTF-8")); // get the bytes
-            StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
+            // Applies sha256 to our data, get the bytes
+            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+
+            // This will contain hash as hexidecimal
+            StringBuffer hexString = new StringBuffer();
 
             for(byte element : hash) {
-                String hex = Integer.toHexString(0xff & element); //change each element into hexadecimal
+
+                // change each element into hexadecimal
+                String hex = Integer.toHexString(0xff & element);
 
                 if(hex.length() == 1){
                     hexString.append('0');
@@ -23,5 +29,42 @@ public class StringUtil {
         catch(Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //Applies ECDSA Signature and returns the result ( as bytes ).
+    public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+        Signature dsa;
+        byte[] output = new byte[0];
+
+        try {
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            dsa.update(strByte);
+
+            byte[] realSig = dsa.sign();
+            output = realSig;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    //Verifies a String signature
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
